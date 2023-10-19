61e180553437a804bf331192e8343ccf77918e97
sonicsuboi <sonicsuboi@gmail.com>
2023-10-19 16:11:15 +0700

    intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import './intro.scss'
+import React from 'react';
+import './intro.scss';
+import bg from '../../assets/image.png';
+import {Link} from 'react-scroll';
 
 const Intro = () => {
     return (
         <div>
             <section id="intro">
 
+                <div className="introContent">
+                <span className="hello">Hello</span>
+                <span className="introText">I'm</span><span className="introName">Alex</span>
+                <p className="introPara">I am a web designer, I</p>
+                    <Link><button type="button"></button></Link>
+                    </div>
+
+<img src={bg} alt="Profile" className='bg'></img>
             </section>
 
         </div>
